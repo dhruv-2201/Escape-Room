89123c9a4ec104d312802bb159f641ecb615ee1f
Dhruv Patel <dhruvppatel2201@gmail.com>
2025-08-31 12:08:39 -0400

    Escape Room

---
+++ escape-room-backend/src/test/java/com/escaperoom/backend/EscapeRoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escaperoom.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeRoomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <h2>Welcome to the Game</h2>;
+}
+
+export default App;
